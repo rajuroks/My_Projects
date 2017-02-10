192b938bd094c1fcee265adc686e4d877f3355fe
rajuroks <raju.roks@gmail.com>
2017-02-10 20:58:07 +0000

    First Commit :)

---
+++ py2.py
@@ -0,0 +1,13 @@
+my_name = "raju"
+age = 35
+height = 74 #Inches
+cm = float(height * 2.54) #height in cms
+weight = 180 #Lbs
+kg = float(weight / 2.2046)
+eyes = "brown"
+teeth = "white"
+hair = "black"
+
+print "Lets talk about %s" % my_name
+print "He's %f cms tall." % cm
+print "He's %f kg's heavy." % kg
