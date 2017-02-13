cfe15c9278c61a3a6a9b5e45fbaf732ec1ca1baf
rajuroks <raju.roks@gmail.com>
2017-02-12 23:32:46 -0500

    change raw_input

--- rawinput1.py
+++ rawinput1.py
@@ -1,9 +1,9 @@
-print "How old are you?"
-age = raw_input()
-print "How tall are you"
-height = raw_input()
-print "how much do you weigh in lbs?"
-weight_lb = raw_input()
+#print "How old are you?"
+age = raw_input("How old are you?")
+#print "How tall are you"
+height = raw_input("How tall are you?")
+#print "how much do you weigh in lbs?"
+weight_lb = raw_input("How much do you weigh?")
 weight_kg = (float (weight_lb) * 0.45359237)
 
 print "So you are %r old %r tall and %r heavy in lbs and %r heavy in kg's." % (age, height, weight_lb, weight_kg)
